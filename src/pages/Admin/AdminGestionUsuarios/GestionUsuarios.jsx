ba3fbf04147714b74a5e8ea87ee1c7570073ed7c
Gabriel Santiago <gabosanti2003@gmail.com>
2024-12-29 15:24:41 -0500

    Admin 2nd Impact

--- src/pages/Admin/AdminGestionUsuarios/GestionUsuarios.jsx
+++ src/pages/Admin/AdminGestionUsuarios/GestionUsuarios.jsx
@@ -1,11 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { FaEdit, FaTrash, FaArrowLeft, FaArrowRight,FaUserPlus } from "react-icons/fa";
 import "./GestionUsuarios.css";
+import { DiVim } from "react-icons/di";
+import { BiColor } from "react-icons/bi";
 
 const GestionUsuarios = () => {
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const rowsPerPage = 10;
+
+  {/* Modal Add */}
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  {/* Modal Edit */}
+  const [isModalOpenEdit, setIsModalOpenEdit] = useState(false);
+
+  const openModalEdit = () => setIsModalOpenEdit(true);
+  const closeModalEdit = () => setIsModalOpenEdit(false);
+
+  {/* Modal Delete */}
+  const [isModalOpenDelete, setIsModalOpenDelete] = useState(false);
+
+  const openModalDelete = () => setIsModalOpenDelete(true);
+  const closeModalDelete = () => setIsModalOpenDelete(false);
+
+    {/* Modal Delete ananidado */}
+    const [isModalOpenNest, setIsModalOpenNest] = useState(false);
+
+    const openModalNest = () => setIsModalOpenNest(true);
+    const closeModalNest = () => setIsModalOpenNest(false);
 
   // Simula la obtencion de datos del backend
   useEffect(() => {
@@ -38,6 +64,7 @@ const GestionUsuarios = () => {
   const totalPages = Math.ceil(users.length / rowsPerPage);
 
   return (
+    
     <div className="user-management-container">
       {/* Sidebar */}
       <div className="sidebar">
@@ -70,13 +97,15 @@ const GestionUsuarios = () => {
                   
                 </td>
                 <td>
-                <button className="add-button"> <FaUserPlus /> </button>
+                <button className="add-button"  onClick={openModal}> <FaUserPlus /> </button>
                 </td>
                 <td></td>
                 <td>
                   
                 </td>
               </tr>
+              
+            
 
               {/* Filas de usuario */}
               {currentRows.map((user) => (
@@ -86,10 +115,10 @@ const GestionUsuarios = () => {
                   <td>{user.email}</td>
                   <td>{user.role}</td>
                   <td>
-                    <button className="edit-button">
+                    <button className="edit-button" onClick={openModalEdit}>
                       <FaEdit />
                     </button>
-                    <button className="delete-button">
+                    <button className="delete-button" onClick={openModalDelete}>
                       <FaTrash />
                     </button>
                   </td>
@@ -98,7 +127,7 @@ const GestionUsuarios = () => {
             </tbody>
           </table>
         </div>
-
+        
         {/* Controles de paginacion */}
         <div className="pagination-controls">
           <button
@@ -118,7 +147,169 @@ const GestionUsuarios = () => {
           </button>
         </div>
       </div>
+      {/* Modal add user */}
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <button className="close-modal" onClick={closeModal}>
+              &times;
+            </button>
+            <h2>Nuevo Usuario</h2>
+            <form className="user-form">
+              <label>
+                Nombre:
+                <input type="text" placeholder="Nombre" required />
+              </label>
+              <label>
+                Usuario:
+                <input type="text" placeholder="Usuario" required />
+              </label>
+              <label>
+                Email:
+                <input type="email" placeholder="Email" required />
+              </label>
+              <div className="password-container">
+                <label>
+                  Contraseña:
+                  <input type="password" placeholder="Contraseña" required />
+                </label>
+                <label>
+                  Confirmar contraseña:
+                  <input
+                    type="password"
+                    placeholder="Confirmar contraseña"
+                    required
+                  />
+                </label>
+              </div>
+              <fieldset>
+                <legend>Rol:</legend>
+                <label>
+                  <input type="radio" name="role" value="Admin" required />
+                  Admin
+                </label>
+                <label>
+                  <input type="radio" name="role" value="Coach" required />
+                  Coach
+                </label>
+                <label>
+                  <input type="radio" name="role" value="Cliente" required />
+                  Cliente
+                </label>
+              </fieldset>
+              <label>
+                Foto de perfil:
+                <input type="file" accept="image/*" />
+              </label>
+              <button type="submit" className="submit-button">
+                REGISTRAR
+              </button>
+            </form>
+          </div>
+        </div>
+        )}
+        {/* Modal Edit user */}
+        {isModalOpenEdit && (
+          <div className="modal-overlay">
+            <div className="modal-content">
+              <button className="close-modal" onClick={closeModalEdit}>
+                &times;
+              </button>
+              <h2>Editar Usuario</h2>
+              <form className="user-form">
+                <label>
+                  Nombre:
+                  <input type="text" placeholder="Nombre" required />
+                </label>
+                <label>
+                  Usuario:
+                  <input type="text" placeholder="Usuario" required />
+                </label>
+                <label>
+                  Email:
+                  <input type="email" placeholder="Email" required />
+                </label>
+                <div className="password-container">
+                  <label>
+                    Contraseña:
+                    <input type="password" placeholder="Contraseña" required />
+                  </label>
+                  <label>
+                    Confirmar contraseña:
+                    <input
+                      type="password"
+                      placeholder="Confirmar contraseña"
+                      required
+                    />
+                  </label>
+                </div>
+                
+                <label>
+                  Foto de perfil:
+                  <input type="file" accept="image/*" />
+                </label>
+                <button type="submit" className="submit-button">
+                  ACTUALIZAR
+                </button>
+              </form>
+            </div>
+          </div>
+
+      )}
+
+      {/* Modal Delete User */}
+      {isModalOpenDelete && (
+          <div className="modal-overlay">
+            <div className="modal-content">
+              <button className="close-modal" onClick={closeModalDelete}>
+                &times;
+              </button>
+              <h2>ESTAS SEGURO DE QUERER ELIMINAR ESTE REGISTRO?</h2>
+              
+                <div className="choice-content">
+                <button className="submit-button" onClick={openModalNest}>
+                  SI
+                </button>
+
+                <button className="submit-button" onClick={closeModalDelete}>
+                  NO
+                </button>
+                </div>
+                <h4 style={{ color: 'red', textAlign: 'center'}}>*NO PODRÁ REVERTIR ESTA ACCION*</h4>
+                 
+            </div>
+          </div>
+
+      )}
+
+      {/* Modal Delete User Nest */}
+      {isModalOpenNest && (
+          <div className="modal-overlay">
+            <div className="modal-content">
+      <div className="header-with-icon">
+        <h2>ELIMINADO</h2>
+        <FaTrash className="icon-trash" />
+      </div>
+      <form className="user-form">
+        <button
+          className="submit-button"
+          onClick={() => {
+            closeModalDelete;
+            closeModalNest
+          }}
+        >
+          CONTINUAR
+        </button>
+      </form>
     </div>
+          </div>
+
+      )}
+    </div>
+
+      
+
+
   );
 };
 
